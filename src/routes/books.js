d18ce1987581b3566411351818e1f54e0a99831d
antunesdvz <g.2008.antunes@gmail.com>
2025-10-27 21:15:07 -0300

    Ultima Alteração

--- src/routes/books.js
+++ src/routes/books.js
@@ -13,18 +13,20 @@ import { adminMiddleware } from "../middleware/admin.js";
 
 const router = Router();
 
+// ✅ Qualquer usuário
 router.get('/', authMiddleware, pegarTodosOsLivros);
 
 router.get('/:id', authMiddleware, pegarLivroPorId);
 
+router.post('/:id/borrow', authMiddleware, emprestarLivro);
+
+router.post('/:id/return', authMiddleware, devolverLivro);
+
+ // 🔒 Apenas admin
 router.post('/', authMiddleware, adminMiddleware, criarLivro);
 
 router.patch("/:id", authMiddleware, adminMiddleware, atualizarLivro);
 
 router.delete('/:id', authMiddleware, adminMiddleware, deletarLivro);
-
-router.post('/:id/borrow', authMiddleware, emprestarLivro);
-
-router.post('/:id/return', authMiddleware, devolverLivro);
 
 export default router;
